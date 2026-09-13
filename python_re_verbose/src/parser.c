0adb51b53c499b7675e36daa2f8342b658b2d035
konomanoasa <238482287+konomanoasa@users.noreply.github.com>
2026-09-13 09:09:56 +0900

    chore: release 0.3.0

--- python_re_verbose/src/parser.c
+++ python_re_verbose/src/parser.c
@@ -12771,7 +12771,7 @@ TS_PUBLIC const TSLanguage *tree_sitter_python_re_verbose(void) {
     .max_reserved_word_set_size = 0,
     .metadata = {
       .major_version = 0,
-      .minor_version = 2,
+      .minor_version = 3,
       .patch_version = 0,
     },
   };
