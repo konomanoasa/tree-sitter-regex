5730895ae3561fc0e98c314801f6d8e6b148bd38
konomanoasa <238482287+konomanoasa@users.noreply.github.com>
2026-09-19 10:09:00 +0900

    chore: release 0.4.0

--- python_re/src/parser.c
+++ python_re/src/parser.c
@@ -14416,7 +14416,7 @@ TS_PUBLIC const TSLanguage *tree_sitter_python_re(void) {
     .max_reserved_word_set_size = 0,
     .metadata = {
       .major_version = 0,
-      .minor_version = 3,
+      .minor_version = 4,
       .patch_version = 0,
     },
   };
--- python_re_verbose/src/parser.c
+++ python_re_verbose/src/parser.c
@@ -13290,7 +13290,7 @@ TS_PUBLIC const TSLanguage *tree_sitter_python_re_verbose(void) {
     .max_reserved_word_set_size = 0,
     .metadata = {
       .major_version = 0,
-      .minor_version = 3,
+      .minor_version = 4,
       .patch_version = 0,
     },
   };
