7c3dda1c01f9d0bb8a8b7556c7e2cfff65d87e6a
konomanoasa <238482287+konomanoasa@users.noreply.github.com>
2026-09-26 21:02:40 +0900

    chore: release 0.8.0

--- posix_bre/src/parser.c
+++ posix_bre/src/parser.c
@@ -2626,7 +2626,7 @@ TS_PUBLIC const TSLanguage *tree_sitter_posix_bre(void) {
     .max_reserved_word_set_size = 0,
     .metadata = {
       .major_version = 0,
-      .minor_version = 7,
+      .minor_version = 8,
       .patch_version = 0,
     },
   };
